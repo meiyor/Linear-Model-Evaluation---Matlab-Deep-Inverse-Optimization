98f00591cadb20163feffdb1dc183e57e18dd1dd
Juan Manuel Mayor Torres <juan.mayortorres@unitn.it>
2020-06-29 17:59:01 -0500

    Update load_anaconda_packages.m

--- load_anaconda_packages.m
+++ load_anaconda_packages.m
@@ -1,6 +1,6 @@
 function load_anaconda_packages()
 %% function collected from stack-overflow blogs on this link https://stackoverflow.com/questions/58252108/how-to-run-a-python-script-with-anaconda-packages-on-matlab
-pyExec = 'D:\Miniconda3\';
+pyExec = "D:\Miniconda3\"; %% here add your Miniconda3 binary path, common Window systems is C:\Miniconda3\ for Unix systems use /Users/yout_user/anaconda/bin/
 pyRoot = fileparts(pyExec);
 p = getenv('PATH');
 p = strsplit(p, ';');

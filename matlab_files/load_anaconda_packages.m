3b9097e9b7016e783aeeb4180e47bb1a296e66ec
Juan Manuel Mayor Torres <juan.mayortorres@unitn.it>
2020-07-02 16:09:39 -0500

    Update load_anaconda_packages.m

--- matlab_files/load_anaconda_packages.m
+++ matlab_files/load_anaconda_packages.m
@@ -5,7 +5,12 @@ pyRoot = fileparts(pyExec);
 p = getenv('PATH');
 p = strsplit(p, ';');
 addToPath = {
-   pyRoot
+   %% include these paths if you are using MacOSX %% 
+   '/usr/local/lib/' 
+   '/usr/local/'
+   '/usr/local/include'
+   %% include these paths if you are using MacOSX, if none of this libraries include the dylibs in Mac please refer to this in Stack Overflow to verify your own https://stackoverflow.com/questions/55600205/where-does-anaconda-install-libraries-on-mac
+   pyRoot   
    fullfile(pyRoot, 'Library', 'mingw-w64', 'bin')
    fullfile(pyRoot, 'Library', 'usr', 'bin')
    fullfile(pyRoot, 'Library', 'bin')
